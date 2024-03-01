60b2625d0aedc1a010619e3e4f88431e6a2144f8
Gabriel Moura <gabrielmoura-cma@hotmail.com>
2024-03-01 20:08:00 -0300

    style: create global style

---
+++ src/global.style.js
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+  html{
+    font-size: 62.5%;
+  }
+
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Nunito', sans-serif;
+  }
+
+  body{
+    width: 100%;
+    min-height:100vh;
+    font-size: 1.6rem;
+  }
+
+`;
