ae5396ddffb7d86b08dbf234f2cc6fbdc1994fb4
Gabriel Moura <gabrielmoura-cma@hotmail.com>
2024-03-03 21:06:55 -0300

    feat: create MainWrapper

---
+++ src/components/MainWrapper.style.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const MainWrapper = styled.main`
+  width: 100%;
+  max-width: 100rem;
+  position: relative;
+  z-index: 10;
+  /* height: 1175px; */
+  background-color: #fff;
+  margin-top: 3rem;
+  border-radius: 0.6rem;
+  border: 0.1rem solid ${({ theme }) => theme.border};
+`;
