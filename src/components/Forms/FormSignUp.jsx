33c4c5ab7843680f406d2c59485c4e3eaaff80d2
Gabriel Moura <gabrielmoura-cma@hotmail.com>
2024-03-06 18:39:25 -0300

    feat: add form validation with zod

--- src/components/Forms/FormSignUp.jsx
+++ src/components/Forms/FormSignUp.jsx
@@ -1,53 +1,142 @@
 import { useState } from "react";
-import { Form, InputWrapper, Input } from "./form.style";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form, InputWrapper, Input, ErrorMsg } from "./form.style";
 import { FiEye, FiEyeOff } from "react-icons/fi";
 
-export default function FormSignUp() {
-  // states para controlar os inputs de senha
-  const [inputType, setInputType] = useState("password");
-  const [visible, setVisble] = useState(false);
+const schema = z
+  .object({
+    firstName: z.string().min(1, "Este campo é obrigatório"),
+    lastName: z.string().min(1, "Este campo é obrigatório"),
+    email: z
+      .string()
+      .min(1, "Este campo é obrigatório")
+      .email("Digite um email válido"),
+    password: z
+      .string()
+      .min(1, "Este campo é obrigatório")
+      .min(6, "Senha deve conter no minimo 6 caracteres"),
+    confirmPassword: z.string().min(1, "Este campo é obrigatório"),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "As senhas não coincidem",
+    path: ["confirmPassword"],
+  });
 
-  function handleVisible() {
-    setInputType((prev) => (prev === "password" ? "text" : "password"));
-    setVisble((prev) => !prev);
+export default function FormSignUp() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: zodResolver(schema) });
+
+  const [password, setPassword] = useState("password");
+  const [confirmPassword, setConfirmPassword] = useState("password");
+
+  function onSubmit(data) {
+    console.log(data);
   }
 
   return (
-    <Form>
+    <Form onSubmit={handleSubmit(onSubmit)} autoComplete="off">
       <div className="names">
         <InputWrapper>
-          <Input type="text" id="firstName" placeholder="Primeiro Nome" />
+          <Input
+            errors={errors?.firstName ? "true" : "false"}
+            type="text"
+            id="firstName"
+            placeholder="Primeiro Nome"
+            {...register("firstName")}
+          />
           <label htmlFor="firstName">Primeiro nome</label>
+
+          {errors?.firstName && <ErrorMsg>{errors.firstName.message}</ErrorMsg>}
         </InputWrapper>
 
         <InputWrapper>
-          <Input type="text" id="lastName" placeholder="Ultimo nome" />
+          <Input
+            errors={errors?.lastName ? "true" : "false"}
+            type="text"
+            id="lastName"
+            placeholder="Ultimo nome"
+            {...register("lastName")}
+          />
           <label htmlFor="lastName">Ultimo nome</label>
+
+          {errors?.lastName && <ErrorMsg>{errors.lastName.message}</ErrorMsg>}
         </InputWrapper>
       </div>
 
       <InputWrapper>
-        <Input type="text" id="email" placeholder="Email" />
+        <Input
+          errors={errors?.email ? "true" : "false"}
+          type="text"
+          id="email"
+          placeholder="Email"
+          {...register("email")}
+        />
         <label htmlFor="email">Email</label>
-      </InputWrapper>
 
-      <InputWrapper>
-        <Input type={inputType} id="password" placeholder="Senha" />
-        <label htmlFor="password">Senha</label>
-
-        <button onClick={handleVisible}>
-          {visible ? <FiEyeOff size={22} /> : <FiEye size={22} />}
-        </button>
+        {errors?.email && <ErrorMsg>{errors.email.message}</ErrorMsg>}
       </InputWrapper>
 
       <InputWrapper>
         <Input
-          type={inputType}
+          errors={errors?.password ? "true" : "false"}
+          type={password}
+          id="password"
+          placeholder="Senha"
+          {...register("password")}
+        />
+        <label htmlFor="password">Senha</label>
+
+        <button
+          onClick={() =>
+            setPassword((prev) => (prev === "password" ? "text" : "password"))
+          }
+          type="button"
+        >
+          {password === "password" ? (
+            <FiEye size={22} />
+          ) : (
+            <FiEyeOff size={22} />
+          )}
+        </button>
+
+        {errors?.password && <ErrorMsg>{errors.password.message}</ErrorMsg>}
+      </InputWrapper>
+
+      <InputWrapper>
+        <Input
+          errors={errors?.confirmPassword ? "true" : "false"}
+          type={confirmPassword}
           id="confirm-password"
           placeholder="Confirmar senha"
+          {...register("confirmPassword")}
         />
         <label htmlFor="confirm-password">Confirmar senha</label>
+
+        <button
+          onClick={() =>
+            setConfirmPassword((prev) =>
+              prev === "password" ? "text" : "password"
+            )
+          }
+          type="button"
+        >
+          {confirmPassword === "password" ? (
+            <FiEye size={22} />
+          ) : (
+            <FiEyeOff size={22} />
+          )}
+        </button>
+        {errors?.confirmPassword && (
+          <ErrorMsg>{errors.confirmPassword.message}</ErrorMsg>
+        )}
       </InputWrapper>
+
+      <button type="submit">Cadastrar</button>
     </Form>
   );
 }
