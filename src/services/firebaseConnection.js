c99a0fb09f89b8d8889ba91af0efab59644c0fa9
Gabriel Moura <gabrielmoura-cma@hotmail.com>
2024-03-09 12:19:51 -0300

    chore: add firebaseConnection config

---
+++ src/services/firebaseConnection.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCllmv0AflQYncS70nsdOdosZOxXElKQ7c",
+  authDomain: "auth-7e2b3.firebaseapp.com",
+  projectId: "auth-7e2b3",
+  storageBucket: "auth-7e2b3.appspot.com",
+  messagingSenderId: "596302532495",
+  appId: "1:596302532495:web:5f8b3d47b37039187ec5fa",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
