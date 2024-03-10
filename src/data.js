5bc8d0f2a45752db6b064e1504151705f63642fc
Gabriel Moura <gabrielmoura-cma@hotmail.com>
2024-03-10 13:45:19 -0300

    refactor: improved structure

--- src/data.js
+++ src/data.js
@@ -1,16 +1,37 @@
-export const avatars = {
-  avatar1:
-    "https://firebasestorage.googleapis.com/v0/b/auth-7e2b3.appspot.com/o/avatars%2Favatar1.jpg?alt=media&token=eeb46bab-d973-4969-877b-d28b2236ca83",
-  avatar2:
-    "https://firebasestorage.googleapis.com/v0/b/auth-7e2b3.appspot.com/o/avatars%2Favatar2.jpg?alt=media&token=64c2f016-e4ef-4174-9201-157a4290b895",
-  avatar3:
-    "https://firebasestorage.googleapis.com/v0/b/auth-7e2b3.appspot.com/o/avatars%2Favatar3.jpg?alt=media&token=7cab2ba9-e3bd-44e1-a8bb-00fc404b5244",
-  avatar4:
-    "https://firebasestorage.googleapis.com/v0/b/auth-7e2b3.appspot.com/o/avatars%2Favatar4.jpg?alt=media&token=75f9ba2b-08d4-4ea2-91ce-49c4de2d9ee0",
-  avatar5:
-    "https://firebasestorage.googleapis.com/v0/b/auth-7e2b3.appspot.com/o/avatars%2Favatar5.jpg?alt=media&token=636fb797-2e12-4c76-adda-10050d32b850",
-  avatar6:
-    "https://firebasestorage.googleapis.com/v0/b/auth-7e2b3.appspot.com/o/avatars%2Favatar6.jpg?alt=media&token=330092f4-b28a-4fa2-a6bc-ef3edcd64a7e",
-  avatar7:
-    "https://firebasestorage.googleapis.com/v0/b/auth-7e2b3.appspot.com/o/avatars%2Favatar7.jpg?alt=media&token=c9e284d5-9cbc-4e88-80bc-114f5b9b4b98",
-};
+export const avatars = [
+  {
+    avatarID: "avatar1",
+    imageLink:
+      "https://firebasestorage.googleapis.com/v0/b/auth-7e2b3.appspot.com/o/avatars%2Favatar1.jpg?alt=media&token=eeb46bab-d973-4969-877b-d28b2236ca83",
+  },
+  {
+    avatarID: "avatar2",
+    imageLink:
+      "https://firebasestorage.googleapis.com/v0/b/auth-7e2b3.appspot.com/o/avatars%2Favatar2.jpg?alt=media&token=64c2f016-e4ef-4174-9201-157a4290b895",
+  },
+  {
+    avatarID: "avatar3",
+    imageLink:
+      "https://firebasestorage.googleapis.com/v0/b/auth-7e2b3.appspot.com/o/avatars%2Favatar3.jpg?alt=media&token=7cab2ba9-e3bd-44e1-a8bb-00fc404b5244",
+  },
+  {
+    avatarID: "avatar4",
+    imageLink:
+      "https://firebasestorage.googleapis.com/v0/b/auth-7e2b3.appspot.com/o/avatars%2Favatar4.jpg?alt=media&token=75f9ba2b-08d4-4ea2-91ce-49c4de2d9ee0",
+  },
+  {
+    avatarID: "avatar5",
+    imageLink:
+      "https://firebasestorage.googleapis.com/v0/b/auth-7e2b3.appspot.com/o/avatars%2Favatar5.jpg?alt=media&token=636fb797-2e12-4c76-adda-10050d32b850",
+  },
+  {
+    avatarID: "avatar6",
+    imageLink:
+      "https://firebasestorage.googleapis.com/v0/b/auth-7e2b3.appspot.com/o/avatars%2Favatar6.jpg?alt=media&token=330092f4-b28a-4fa2-a6bc-ef3edcd64a7e",
+  },
+  {
+    avatarID: "avatar7",
+    imageLink:
+      "https://firebasestorage.googleapis.com/v0/b/auth-7e2b3.appspot.com/o/avatars%2Favatar7.jpg?alt=media&token=c9e284d5-9cbc-4e88-80bc-114f5b9b4b98",
+  },
+];
