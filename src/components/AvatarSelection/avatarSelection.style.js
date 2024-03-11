618cd334af2edc496ab3cf1933625e5ffa3047cf
Gabriel Moura <gabrielmoura-cma@hotmail.com>
2024-03-11 17:51:55 -0300

    style: add styles

--- src/components/AvatarSelection/avatarSelection.style.js
+++ src/components/AvatarSelection/avatarSelection.style.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export const AvatarSelectionWrapper = styled.div`
   margin-top: 5.2rem;
@@ -47,27 +47,103 @@ export const Label = styled.label`
   cursor: pointer;
 
   &:hover {
-    outline: solid 0.4rem #5acf5f;
+    outline: solid 0.4rem #ccc;
   }
 `;
 
 export const UploadImageButton = styled.button`
-  background-color: ${({ theme }) => theme.blue};
+  position: relative;
   background-color: #ccc;
   transition: all 0.3s ease;
+  cursor: pointer;
+  overflow: hidden;
+  box-sizing: content-box;
+  border: solid 0.1rem transparent;
+  outline: solid 0.4rem transparent;
 
-  img {
+  .icon {
     width: 3.2rem;
-    transition: all 0.3s ease;
+  }
+
+  .profile-image {
+    width: 7rem;
+    height: 7rem;
+    object-fit: cover;
   }
 
   &:hover {
     transform: scale(0.9);
-    background-color: #5acf5f;
+    background-color: ${({ theme }) => theme.blue};
   }
+
+  ${(props) =>
+    props.hasimage === "true" &&
+    css`
+      transition: none;
+      background-color: #fff;
+      &:hover {
+        transform: none;
+        border-color: #fff;
+        outline-color: #ccc;
+      }
+
+      ${LabelImage} {
+        opacity: 1;
+      }
+
+      ${InputImage} {
+        z-index: -1;
+      }
+    `}
+
+  ${(props) =>
+    props.isselected === "true" &&
+    css`
+      outline-color: #5acf5f;
+      &:hover {
+        outline-color: #5acf5f;
+      }
+    `}
+`;
+
+export const LabelImage = styled.label`
+  font-size: 1.2rem;
+  width: 6.6rem;
+  position: absolute;
+  bottom: 0rem;
+  right: -1.3rem;
+  padding: 0.4rem 0;
+  background-color: rgba(0, 0, 0, 0.4);
+  transform: rotate(-35deg);
+  cursor: pointer;
+  opacity: 0;
+
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.7);
+  }
+
+  svg {
+    transform: rotate(28deg);
+    /* transform: translate(4px, ); */
+  }
+`;
+
+export const InputImage = styled.input`
+  width: 7rem;
+  height: 7rem;
+  position: absolute;
+  top: 0;
+  left: 0;
+  opacity: 0;
+  border-radius: 50%;
+  cursor: pointer;
 `;
 
 export const InputRadio = styled.input`
   width: 0px;
   opacity: 0;
+
+  &:checked + ${Label} {
+    outline: solid 0.4rem #5acf5f;
+  }
 `;
