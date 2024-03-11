b5860749c37602d41f3a64ba84bf99603cafc69b
Gabriel Moura <gabrielmoura-cma@hotmail.com>
2024-03-11 17:50:16 -0300

    refactor: schema placed in a separate file

---
+++ src/schema.js
@@ -0,0 +1,20 @@
+import { z } from "zod";
+
+export const schema = z
+  .object({
+    firstName: z.string().min(1, "Este campo é obrigatório"),
+    lastName: z.string().min(1, "Este campo é obrigatório"),
+    email: z
+      .string()
+      .min(1, "Este campo é obrigatório")
+      .email("Digite um email válido"),
+    password: z
+      .string()
+      .min(1, "Este campo é obrigatório")
+      .min(6, "Senha deve conter no minimo 6 caracteres"),
+    confirmPassword: z.string().min(1, "Este campo é obrigatório"),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "As senhas não coincidem",
+    path: ["confirmPassword"],
+  });
