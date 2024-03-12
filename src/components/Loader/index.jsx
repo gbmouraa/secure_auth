c1969f363192dec1f45638d572e984459a0e75d9
Gabriel Moura <gabrielmoura-cma@hotmail.com>
2024-03-12 13:59:36 -0300

    feat: add Loader

---
+++ src/components/Loader/index.jsx
@@ -0,0 +1,13 @@
+import "./loader.style.css";
+
+export default function Loader() {
+  return (
+    <div className="loading">
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  );
+}
