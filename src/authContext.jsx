cc870212f8a8e763ce26a2d100c889358f1c1401
Gabriel Moura <gabrielmoura-cma@hotmail.com>
2024-03-14 19:23:31 -0300

    feat: add useEffect to check if user is logged in, refactored the signUp function and add signIn

--- src/authContext.jsx
+++ src/authContext.jsx
@@ -1,10 +1,12 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 import { db, auth, storage } from "./services/firebaseConnection";
-import { createUserWithEmailAndPassword } from "firebase/auth";
-import { collection, doc, setDoc, addDoc } from "firebase/firestore";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { collection, doc, addDoc, setDoc, getDoc } from "firebase/firestore";
 import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
-import { string } from "zod";
-
+import { useNavigate } from "react-router-dom";
 export const AuthContext = createContext({});
 
 export function AuthProvider({ children }) {
@@ -12,15 +14,27 @@ export function AuthProvider({ children }) {
   const [avatarSelected, setAvatarSelected] = useState("");
   // foto de perfil que é enviada ao firebase
   const [imageFile, setImageFile] = useState(null);
-  const [isSigned, setIsSigned] = useState(false);
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    function checkUserStorage() {
+      const userStorage = localStorage.getItem("_secureAuth");
+      if (userStorage) setUser(JSON.parse(userStorage));
+      setLoading(false);
+    }
+
+    checkUserStorage();
+  }, []);
 
   async function signUp(userData) {
     setLoading(true);
 
-    // flag para definir se será necessário fazer o upload de uma imagem par o firebase
+    // flag para definir se será necessário fazer o upload de uma imagem para o firebase
     let sendImage = false;
     let avatar = "";
+
     if (avatarSelected && avatarSelected !== "profileImage") {
       avatar = avatarSelected.imageLink;
     } else if (avatarSelected === "profileImage") {
@@ -34,7 +48,7 @@ export function AuthProvider({ children }) {
     )
       .then(async (value) => {
         const userCredentials = value.user;
-        const docRef = collection(db, "users");
+        const docRef = doc(db, "users", userCredentials.uid);
 
         if (sendImage) {
           await uploadFile(userCredentials.uid).then((dowloadURL) => {
@@ -42,23 +56,26 @@ export function AuthProvider({ children }) {
           });
         }
 
-        await addDoc(docRef, {
+        await setDoc(docRef, {
           userID: userCredentials.uid,
-          name: userData.firstName,
+          firstName: userData.firstName,
+          lastName: userData.lastName,
           email: userCredentials.email,
           avatarURL: avatar,
         });
 
         let data = {
           userID: userCredentials.uid,
-          name: userData.name,
+          firstName: userData.firstName,
+          lastName: userData.lastName,
           email: userCredentials.email,
           avatarURL: avatar,
         };
 
         setUser(data);
-        setIsSigned(true);
-        console.log("success");
+        userStorage(data);
+        setAvatarSelected("");
+        navigate("/profile");
       })
       .catch((error) => {
         console.log(error + error.code);
@@ -66,10 +83,43 @@ export function AuthProvider({ children }) {
       .finally(() => setLoading(false));
   }
 
-  // envia e retorna o link da imagem de usuário no firebas
+  async function signIn(email, password) {
+    await signInWithEmailAndPassword(auth, email, password)
+      .then(async (snapshot) => {
+        const uid = snapshot.user.uid;
+        const docRef = doc(db, "users", uid);
+
+        await getDoc(docRef).then((docSnap) => {
+          const userData = docSnap.data();
+
+          const data = {
+            userID: uid,
+            firstName: userData.firstName,
+            lastName: userData.lastName,
+            email: userData.email,
+            avatarURL: userData.avatarURL,
+          };
+
+          userStorage(user);
+          setUser(data);
+          navigate("/profile");
+        });
+      })
+      .catch((error) => {
+        console.log(error + error.code);
+      })
+      .finally(() => setLoading(false));
+  }
+
+  function userStorage(user) {
+    localStorage.setItem("_secureAuth", JSON.stringify(user));
+  }
+
+  // envia e retorna o link da imagem de usuário no firebase
   async function uploadFile(userID) {
     const uploadRef = ref(storage, `image/${userID}/${imageFile.name}`);
 
+    // ** refatorar
     return uploadBytes(uploadRef, imageFile)
       .then(async (value) => {
         return getDownloadURL(value.ref).then((dowloadURL) => {
@@ -86,12 +136,13 @@ export function AuthProvider({ children }) {
       value={{
         user,
         loading,
-        isSigned,
+        isSigned: !!user,
         signUp,
         avatarSelected,
         setAvatarSelected,
         imageFile,
         setImageFile,
+        signIn,
       }}
     >
       {children}
