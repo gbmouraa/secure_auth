77cbb2b8b2db976427ac2c1924feb40b9b241c1d
Gabriel Moura <gabrielmoura-cma@hotmail.com>
2024-03-14 19:27:10 -0300

    refactor: .

--- src/routes/Private.jsx
+++ src/routes/Private.jsx
@@ -3,7 +3,11 @@ import { Navigate } from "react-router-dom";
 import { AuthContext } from "../authContext";
 
 export default function Private({ children }) {
-  const { isSigned } = useContext(AuthContext);
+  const { isSigned, loading } = useContext(AuthContext);
+
+  if (loading) {
+    return <div></div>;
+  }
 
   if (!isSigned) {
     return <Navigate to="/" />;
