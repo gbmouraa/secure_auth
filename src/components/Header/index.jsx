b7ff82cd9eedda9a7732e7cbf3eeb303b9c0a25c
Gabriel Moura <gabrielmoura-cma@hotmail.com>
2024-03-15 12:28:02 -0300

    feat: add navigation between login and register

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,13 +1,32 @@
+import { useContext } from "react";
+import { useLocation } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../authContext";
 import { HeaderWrapper, ActionButton } from "./header.style";
 import { Logo } from "../Logo.style";
 
 export default function Header() {
+  const { isSigned } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  function handleAuth() {
+    if (location.pathname === "/") {
+      navigate("/login");
+      return;
+    }
+
+    navigate("/");
+  }
+
   return (
     <HeaderWrapper>
       <Logo>SecureAuth</Logo>
-      <ActionButton>
-        Login <div />
-      </ActionButton>
+      {!isSigned && (
+        <ActionButton onClick={handleAuth}>
+          {location.pathname === "/" ? "Entrar" : "Cadastre-se"} <div />
+        </ActionButton>
+      )}
     </HeaderWrapper>
   );
 }
