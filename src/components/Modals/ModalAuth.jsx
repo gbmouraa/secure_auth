bbe1256090c5d16858ccf29e5b253ec31974f369
Gabriel Moura <gabrielmoura-cma@hotmail.com>
2024-03-16 16:16:37 -0300

    chore: changed the file name

---
+++ src/components/Modals/ModalAuth.jsx
@@ -0,0 +1,65 @@
+import { useEffect, useState } from "react";
+import {
+  ModalWrapper,
+  Modal,
+  Illustration,
+  RegisterSuccess,
+  Loading,
+  Msg,
+} from "./modal.style";
+import Loader from "../Loader";
+import checkIcon from "../../assets/check.png";
+
+export default function ModalAuth({ authAction }) {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    function animation() {
+      setTimeout(() => {
+        setLoading(false);
+      }, 3000);
+    }
+    animation();
+  }, []);
+
+  return (
+    <ModalWrapper
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+    >
+      <Modal>
+        <Loading opacity={loading ? "1" : "0"}>
+          <Loader />
+          <p>Aguarde enquanto efetuamos seu {authAction}.</p>
+        </Loading>
+        {!loading && (
+          <RegisterSuccess>
+            <Illustration
+              src={checkIcon}
+              alt="illustration"
+              initial={{ opacity: 0, scale: 0.5 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{
+                duration: 0.8,
+                delay: 0.5,
+                ease: [0, 0.71, 0.2, 1.01],
+              }}
+            />
+            <Msg
+              initial={{ opacity: 0, scale: 0.5 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{
+                duration: 0.8,
+                delay: 0.5,
+                ease: [0, 0.71, 0.2, 1.01],
+              }}
+            >
+              <span className="upper">{authAction}</span> efetuado com sucesso!
+              &#x1F389;
+            </Msg>
+          </RegisterSuccess>
+        )}
+      </Modal>
+    </ModalWrapper>
+  );
+}
---
+++ src/components/Modals/modal.style.js
@@ -0,0 +1,57 @@
+import styled from "styled-components";
+import { motion } from "framer-motion";
+
+export const ModalWrapper = styled(motion.div)`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(0, 0, 0, 0.2);
+  z-index: 99999;
+  display: flex;
+  justify-items: center;
+  align-items: center;
+  padding: 0 1.2rem;
+`;
+export const Modal = styled(motion.div)`
+  width: 100%;
+  max-width: 60rem;
+  background-color: #fff;
+  border-radius: 0.6rem;
+  padding: 6rem 4rem;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  p {
+    font-size: 1.6rem;
+    margin-top: 2rem;
+    font-weight: 500;
+  }
+`;
+export const Loading = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  opacity: ${(props) => props.opacity};
+`;
+export const Illustration = styled(motion.img)`
+  display: block;
+  margin-bottom: 2rem;
+  width: 8rem;
+`;
+export const RegisterSuccess = styled(motion.div)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: absolute;
+`;
+
+export const Msg = styled(motion.p)`
+  .upper {
+    text-transform: capitalize;
+  }
+`;
