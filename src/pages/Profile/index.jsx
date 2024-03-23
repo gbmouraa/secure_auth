217a163b9b4e4a4d51f8b89b48ea8599d580d287
Gabriel Moura <gabrielmoura-cma@hotmail.com>
2024-03-22 22:54:00 -0300

    feat: add ModalLogout

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -9,10 +9,12 @@ import {
 } from "./profile.style";
 import userIcon from "../../assets/images/user.svg";
 import AvatarSelection from "../../components/AvatarSelection";
+import ModalLogout from "../../components/Modals/ModalLogout";
 
 export default function Profile() {
-  const { logOut, user, setAvatarSelected } = useContext(UserContext);
+  const { user, setAvatarSelected } = useContext(UserContext);
   const [isEditing, setIsEditing] = useState(false);
+  const [showModalLogout, setShowModalLogout] = useState(false);
 
   function closeAvatarSelection() {
     setIsEditing(false);
@@ -30,20 +32,27 @@ export default function Profile() {
             ) : (
               <img src={user.avatarURL} alt="Avatar" />
             )}
-            <button onClick={() => setIsEditing(true)}>Editar</button>
+            {!user.thirdPartyAuth && (
+              <button onClick={() => setIsEditing(true)}>Editar</button>
+            )}
           </div>
         </Avatar>
         <UserInfo>
           <span>{`${user.firstName} ${user.lastName}`}</span>
           <span>{user.email}</span>
         </UserInfo>
-        <LogoutButton onClick={logOut}>Sair</LogoutButton>
+        <LogoutButton onClick={() => setShowModalLogout(true)}>
+          Sair
+        </LogoutButton>
       </ProfileWrapper>
       {isEditing && (
         <AvatarSelection
           profileEditing={true}
           closeModal={closeAvatarSelection}
         />
+      )}
+      {showModalLogout && (
+        <ModalLogout closeModal={() => setShowModalLogout(false)} />
       )}
     </ProfileContainer>
   );
