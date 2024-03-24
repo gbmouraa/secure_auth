260462cf06ff9c5d611c9461b67c1451c2840380
Gabriel Moura <gabrielmoura-cma@hotmail.com>
2024-03-24 13:15:08 -0300

    feat: add login schema

--- src/validations/schema.js
+++ src/validations/schema.js
@@ -1,5 +1,6 @@
 import { z } from "zod";
 
+// register
 export const schema = z
   .object({
     firstName: z.string().min(1, "Este campo é obrigatório"),
@@ -18,3 +19,11 @@ export const schema = z
     message: "As senhas não coincidem",
     path: ["confirmPassword"],
   });
+
+export const loginSchema = z.object({
+  email: z
+    .string()
+    .min(1, "Este campo é obrigatório")
+    .email("Digite um email válido"),
+  password: z.string().min(1, "Este campo é obrigatório"),
+});
