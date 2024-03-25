0001e0299dcd94d390791d076ccfa94242b3e73a
Gabriel Moura <gabrielmoura-cma@hotmail.com>
2024-03-25 20:55:54 -0300

    feat: .

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,37 +1,34 @@
 import { useContext, useState } from "react";
 import { UserContext } from "../../userContext";
-import { MainWrapper } from "../../components/MainWrapper.style";
+import { LoginWrapper } from "./login.style";
 import ModalAuth from "../../components/Modals/ModalAuth";
+import { AuthOptions, LoginAnchor } from "../Register/register.style";
+import ThirdPartyLogin from "../../components/ThirdPartyLogin";
+import { MainWrapper } from "../../components/MainWrapper.style";
+import { RegisterContainer } from "../Register/register.style";
+import FormLogin from "../../components/Forms/FormLogin";
 
 export default function LogIn() {
-  const { signIn, loading } = useContext(UserContext);
-
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  function handleLogin() {
-    signIn(email, password);
-  }
+  const { loading } = useContext(UserContext);
 
   return (
-    <MainWrapper>
-      <div>
-        <label>Email</label>
-        <input
-          type="text"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <label>Senha</label>
-        <input
-          type="text"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button onClick={handleLogin}>Entrar</button>
-      </div>
-
-      {loading && <ModalAuth authAction="login" />}
-    </MainWrapper>
+    <RegisterContainer>
+      <MainWrapper style={{ maxWidth: "600px" }} className="main-wrapper-login">
+        <LoginWrapper>
+          <p>Login</p>
+          <AuthOptions>
+            <p className="sm">
+              Não possui uma conta?{" "}
+              <LoginAnchor to="/">Cadastre-se</LoginAnchor>
+            </p>
+            <p className="sm">Ou continue com</p>
+            <ThirdPartyLogin />
+          </AuthOptions>
+          <FormLogin />
+          {/* <span>Esqueceu sua senha?</span> */}
+          {loading && <ModalAuth authAction="login" />}
+        </LoginWrapper>
+      </MainWrapper>
+    </RegisterContainer>
   );
 }
--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -1,12 +1,19 @@
 import { useContext } from "react";
 import { Navigate } from "react-router-dom";
 import { UserContext } from "../../userContext";
-import { RegisterContainer } from "./register.style";
+import {
+  RegisterContainer,
+  SignUpWrapper,
+  AuthOptions,
+  LoginAnchor,
+} from "./register.style";
 import { MainWrapper } from "../../components/MainWrapper.style";
-import SignUp from "../../components/SignUp";
+import ThirdPartyLogin from "../../components/ThirdPartyLogin";
+import FormSignUp from "../../components/Forms/FormSignUp";
+import ModalAuth from "../../components/Modals/ModalAuth";
 
 export default function Register() {
-  const { isSigned } = useContext(UserContext);
+  const { isSigned, loading } = useContext(UserContext);
 
   if (isSigned) {
     return <Navigate to="/profile" />;
@@ -15,7 +22,20 @@ export default function Register() {
   return (
     <RegisterContainer>
       <MainWrapper>
-        <SignUp />
+        <SignUpWrapper>
+          <p>Criar nova conta</p>
+          <AuthOptions>
+            <p className="sm">
+              Já possui uma conta?{" "}
+              <LoginAnchor to="/login">Faça login</LoginAnchor>
+            </p>
+            <p className="sm">Ou continue com</p>
+            <ThirdPartyLogin />
+          </AuthOptions>
+          <FormSignUp />
+
+          {loading && <ModalAuth authAction="cadastro" />}
+        </SignUpWrapper>
       </MainWrapper>
     </RegisterContainer>
   );
