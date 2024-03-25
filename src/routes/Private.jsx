75fdc6038a884f2311a71ccb266e18e42139d449
Gabriel Moura <gabrielmoura-cma@hotmail.com>
2024-03-25 20:51:57 -0300

    style: add loader

--- src/routes/Private.jsx
+++ src/routes/Private.jsx
@@ -1,12 +1,19 @@
 import { useContext } from "react";
 import { Navigate } from "react-router-dom";
 import { UserContext } from "../userContext";
+import Loader from "../components/Loader";
 
 export default function Private({ children }) {
   const { isSigned, loading } = useContext(UserContext);
 
   if (loading) {
-    return <div></div>;
+    return (
+      <div className="loader">
+        <div className="loader-box">
+          <Loader />
+        </div>
+      </div>
+    );
   }
 
   if (!isSigned) {
