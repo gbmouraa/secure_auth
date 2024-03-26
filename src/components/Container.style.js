395972dd8d92fde9ad1739c1e83251d8c27d5f3d
Gabriel Moura <gabrielmoura-cma@hotmail.com>
2024-03-25 22:09:50 -0300

    style: fix bg path

--- src/components/Container.style.js
+++ src/components/Container.style.js
@@ -15,7 +15,7 @@ export const Container = styled.div`
     left: 0;
     width: 100%;
     height: 20.2rem;
-    background-image: url("./src/assets/images/wave-top.svg");
+    background-image: url("https://firebasestorage.googleapis.com/v0/b/auth-7e2b3.appspot.com/o/image%2Fwaves%2Fwave-top.svg?alt=media&token=14eaaa06-b862-4903-82fe-8ec11f577f12");
     background-size: cover;
     background-repeat: no-repeat;
     background-position: center bottom;
@@ -33,7 +33,7 @@ export const Container = styled.div`
     left: 0;
     width: 100%;
     height: 30%;
-    background-image: url("./src/assets/images/wave-bottom.svg");
+    background-image: url("https://firebasestorage.googleapis.com/v0/b/auth-7e2b3.appspot.com/o/image%2Fwaves%2Fwave-bottom.svg?alt=media&token=b2ad22b7-dbfd-4b2c-b377-9919cdcb94bc");
     background-size: cover;
     background-repeat: no-repeat;
     background-position: right top;
